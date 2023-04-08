12fad2ad5ef4e7cb59635ad7cf2829ccf0197684
Max Ray <maxxray@yahoo.com>
2023-04-08 16:30:03 -0600

    commit

---
+++ index.js
@@ -0,0 +1,85 @@
+
+var change = $("box1:first");
+change.click(function() {
+    change.toggleClass("darkMode")
+});
+
+const colorChange = document.querySelector('.box1');
+colorChange.addEventListener('click', () => {
+  colorChange.classList.toggle('darkMode');
+});
+
+const colorChange2 = document.querySelector('.box2');
+colorChange2.addEventListener('click', () => {
+  colorChange2.classList.toggle('darkMode');
+});
+
+const colorChange3 = document.querySelector('.box3');
+colorChange3.addEventListener('click', () => {
+  colorChange3.classList.toggle('darkMode');
+});
+
+const colorChange4 = document.querySelector('.box4');
+colorChange4.addEventListener('click', () => {
+  colorChange4.classList.toggle('darkMode');
+});
+
+const colorChange5 = document.querySelector('.box5');
+colorChange5.addEventListener('click', () => {
+  colorChange5.classList.toggle('darkMode');
+});
+
+const colorChange6 = document.querySelector('.box6');
+colorChange6.addEventListener('click', () => {
+  colorChange6.classList.toggle('darkMode');
+});
+
+const colorChange7 = document.querySelector('.box7');
+colorChange7.addEventListener('click', () => {
+  colorChange7.classList.toggle('darkMode');
+});
+
+const colorChange8 = document.querySelector('.box8');
+colorChange8.addEventListener('click', () => {
+  colorChange8.classList.toggle('darkMode');
+});
+
+const colorChange9 = document.querySelector('.box9');
+colorChange9.addEventListener('click', () => {
+  colorChange9.classList.toggle('darkMode');
+});
+
+const colorChange10 = document.querySelector('.box10');
+colorChange10.addEventListener('click', () => {
+  colorChange10.classList.toggle('darkMode');
+});
+
+const colorChange11 = document.querySelector('.box11');
+colorChange11.addEventListener('click', () => {
+  colorChange11.classList.toggle('darkMode');
+});
+
+const colorChange12 = document.querySelector('.box12');
+colorChange12.addEventListener('click', () => {
+  colorChange12.classList.toggle('darkMode');
+});
+
+const colorChange13 = document.querySelector('.box13');
+colorChange13.addEventListener('click', () => {
+  colorChange13.classList.toggle('darkMode');
+});
+
+const colorChange14 = document.querySelector('.box14');
+colorChange14.addEventListener('click', () => {
+  colorChange14.classList.toggle('darkMode');
+});
+
+const colorChange15 = document.querySelector('.box15');
+colorChange15.addEventListener('click', () => {
+  colorChange15.classList.toggle('darkMode');
+});
+
+const colorChange16 = document.querySelector('.box16');
+colorChange16.addEventListener('click', () => {
+  colorChange16.classList.toggle('darkMode');
+});
